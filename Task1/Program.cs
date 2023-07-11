e7232d0f8f3f054616efbde155aa371b04ddf916
Sergey Antipov <antipovsergey84@gmail.com>
2023-07-11 23:22:42 +0300

    initial commit

---
+++ Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int Recursion(int M, int N)
+{
+    if (M == N)
+    {
+        return N;
+    }
+    return M + Recursion(M + 1, N);
+}
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+////////////////////////////////////////////////////////////
+
+int firstNum = ReadInt("Введите начальное число: ");
+int lastNum = ReadInt("Введите конечное число: ");
+if (firstNum > lastNum) { System.Console.WriteLine("Начальное число не должно быть больше конечного"); return; }
+System.Console.WriteLine(Recursion(firstNum, lastNum));
---
+++ Task2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int AkkerFunction(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    if (m > 0 && n == 0)
+    {
+        return AkkerFunction(m - 1, 1);
+    }
+    if (m > 0 && n > 0)
+    {
+        return AkkerFunction(m - 1, AkkerFunction(m, n - 1));
+    }
+    return AkkerFunction(m, n);
+}
+
+////////////////////////////////////////////////////////////
+
+int firstNum = ReadInt("Введите первое целое положительное число: ");
+int secondNum = ReadInt("Введите второе целое положительное число: ");
+if (firstNum < 0 || secondNum < 0) { System.Console.WriteLine("Оба числа должны быть положительными"); return; }
+System.Console.WriteLine();
+System.Console.WriteLine($"Akkerman Function ({firstNum},{secondNum}) = {AkkerFunction(firstNum, secondNum)}");
